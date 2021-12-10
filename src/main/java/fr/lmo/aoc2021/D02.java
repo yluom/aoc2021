9c40ce081af1f0b5e917128b1e9f2d59ce9a10e4
Léo Mouly <leo.mouly@gmail.com>
2021-12-11 00:10:14 +0100

    D2 dumb 2d star draft

--- src/main/java/fr/lmo/aoc2021/D02.java
+++ src/main/java/fr/lmo/aoc2021/D02.java
@@ -5,23 +5,26 @@ import java.util.stream.Stream;
 public class D02 extends AoCLMO {
 
     public void run() {
-        System.out.println("Test file increases : " + firstStar(stream(getTestInputPath())));
-        System.out.println("Real file increases : " + firstStar((stream(getInputPath()))));
-/*
-        System.out.println("Test file increases window : " + findIncreases(stream(getTestInputPath(), Integer::valueOf), 3));
-        System.out.println("Real file increases window : " + findIncreases(stream(getInputPath(), Integer::valueOf), 3));*/
+        System.out.println("First star test : " + firstStar(stream(getTestInputPath())));
+        System.out.println("First star input : " + firstStar((stream(getInputPath()))));
+
+        System.out.println("Second star test : " + secondStar(stream(getTestInputPath())));
+        System.out.println("Second star input : " + secondStar((stream(getInputPath()))));
+
     }
 
     public record Move(String direction, int number) {
     }
 
     private static class Position {
+        public int aim;
         public int x;
         public int y;
 
-        Position(int x, int y) {
+        Position(int x, int y, int aim) {
             x = x;
             y = y;
+            aim = aim;
         }
 
     }
@@ -29,15 +32,34 @@ public class D02 extends AoCLMO {
     private int firstStar(Stream<String> stream) {
         Stream<Move> moves = stream.map(line -> new Move(line.split(" ")[0], Integer.parseInt(line.split(" ")[1])));
 
-        final Position p = new Position(0, 0);
+        final Position p = new Position(0, 0, 0);
         moves.forEach(m ->
         {
             if (m.direction.equals("forward")) {
-                p.x = p.x + m.number;
+                p.x += m.number;
             } else if (m.direction.equals("up")) {
-                p.y = p.y - m.number;
+                p.y -= m.number;
             } else if (m.direction.equals("down")) {
-                p.y = p.y + m.number;
+                p.y += m.number;
+            }
+        });
+
+        return p.x * p.y;
+    }
+
+    private int secondStar(Stream<String> stream) {
+        Stream<Move> moves = stream.map(line -> new Move(line.split(" ")[0], Integer.parseInt(line.split(" ")[1])));
+
+        final Position p = new Position(0, 0, 0);
+        moves.forEach(m ->
+        {
+            if (m.direction.equals("forward")) {
+                p.x += m.number;
+                p.y += p.aim*m.number;
+            } else if (m.direction.equals("up")) {
+                p.aim -= m.number;
+            } else if (m.direction.equals("down")) {
+                p.aim += m.number;
             }
         });
 
